4bd3e2b87a31045ca63ed8cb34f9c801478762e2
toxicbishop <92860129+toxicbishop@users.noreply.github.com>
2026-04-02 19:18:45 +0530

    Remade this

---
+++ app.py
@@ -0,0 +1,58 @@
+import requests
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "Welcome to the Test API",
+        "endpoints": [
+            "/posts",
+            "/comments",
+            "/albums"
+        ],
+        "status": "online"
+    })
+
+@app.route('/posts')
+def get_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "successfully triggered auto deployment",
+        "status_code": 200
+    })
+
+@app.route('/comments')
+def get_comments():
+    url = "https://jsonplaceholder.typicode.com/comments"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
+    })
+
+@app.route('/albums')
+def get_albums():
+    url = "https://jsonplaceholder.typicode.com/albums"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
